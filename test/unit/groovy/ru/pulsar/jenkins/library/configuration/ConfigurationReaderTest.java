f445fda572463e2d9ef335f8fff9c281e9ff4db8
v.belyaev <v.belyaev@1ceasy.ru>
2024-11-19 11:16:57 +0300

    fix tests

--- test/unit/groovy/ru/pulsar/jenkins/library/configuration/ConfigurationReaderTest.java
+++ test/unit/groovy/ru/pulsar/jenkins/library/configuration/ConfigurationReaderTest.java
@@ -34,9 +34,7 @@ class ConfigurationReaderTest {
 
     // then
     assertThat(jobConfiguration.getV8version()).isEqualTo("8.3.14.1944");
-    assertThat(jobConfiguration.getEdtVersion()).isEqualTo("2021.3.4:x86_64");
-    assertThat(jobConfiguration.getPathEDTCliDef()).isEqualTo("/opt/1C/1CE/components/");
-    
+    assertThat(jobConfiguration.getEdtVersion()).isEqualTo("2021.3.4:x86_64");    
 
     assertThat(jobConfiguration.getSonarQubeOptions().getSonarScannerToolName()).isEqualTo("sonar-scanner");
     assertThat(jobConfiguration.getSonarQubeOptions().getSonarQubeInstallation()).isEqualTo("qa");
@@ -113,38 +111,6 @@ class ConfigurationReaderTest {
 
     // then
     assertThat(jobConfiguration.edtAgentLabel()).isEqualTo("edt@2021.3.4:x86_64");
-  }
-
-  @Test
-  void testuseEDTCli() throws IOException {
-    // given
-    String config = IOUtils.resourceToString(
-            "jobConfiguration.json",
-            StandardCharsets.UTF_8,
-            this.getClass().getClassLoader()
-    );
-
-    // when
-    JobConfiguration jobConfiguration = ConfigurationReader.create(config);
-
-    // then
-    assertThat(jobConfiguration.useEDTCli()).isFalse();
-  }
-
-  @Test
-  void testgetEDTPath() throws IOException {
-    // given
-    String config = IOUtils.resourceToString(
-            "jobConfiguration.json",
-            StandardCharsets.UTF_8,
-            this.getClass().getClassLoader()
-    );
-
-    // when
-    JobConfiguration jobConfiguration = ConfigurationReader.create(config);
-
-    // then
-    assertThat(jobConfiguration.getEDTPath()).isEqualTo("/opt/1C/1CE/components/");
   }
 
   @Disabled

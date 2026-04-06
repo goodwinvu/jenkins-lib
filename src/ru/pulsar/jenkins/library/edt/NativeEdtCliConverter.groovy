84938fb5d1ce7e1f4f460fbdfccb8b13955cb8ad
v.belyaev <v.belyaev@1ceasy.ru>
2026-04-06 11:13:34 +0300

    Отключаем проверку и ппд при операциях ЕДТ

--- src/ru/pulsar/jenkins/library/edt/NativeEdtCliConverter.groovy
+++ src/ru/pulsar/jenkins/library/edt/NativeEdtCliConverter.groovy
@@ -28,7 +28,7 @@ class NativeEdtCliConverter implements IEdtCliEngine {
         steps.deleteDir(configurationRoot)
 
         def projectDir = FileUtils.getFilePath("$env.WORKSPACE/$srcDir").getRemote()
-        def edtcliCommand = "1cedtcli -data \"$projectWorkspaceDir\" -vmargs -Xmx8g -command export --configuration-files \"$configurationRootFullPath\" --project \"$projectDir\""
+        def edtcliCommand = "1cedtcli -data \"$projectWorkspaceDir\" -vmargs -Xmx8g -DdisableProjectChecks=true -DdisableFTS=true -command export --configuration-files \"$configurationRootFullPath\" --project \"$projectDir\""
 
         steps.cmd(edtcliCommand)
 
@@ -53,7 +53,7 @@ class NativeEdtCliConverter implements IEdtCliEngine {
             def currentExtensionWorkspaceDir = FileUtils.getFilePath("$workspaceDir/cfe/${it.name}")
             def currentExtensionProjectDir = FileUtils.getFilePath("$env.WORKSPACE/${it.path}").getRemote()
 
-            def edtcliCommand = "1cedtcli -data \"$currentExtensionWorkspaceDir\" -vmargs -Xmx8g -command export --configuration-files \"$extensionRoot/${it.name}\" --project \"$currentExtensionProjectDir\""
+            def edtcliCommand = "1cedtcli -data \"$currentExtensionWorkspaceDir\" -vmargs -Xmx8g -DdisableProjectChecks=true -DdisableFTS=true -command export --configuration-files \"$extensionRoot/${it.name}\" --project \"$currentExtensionProjectDir\""
 
             steps.cmd(edtcliCommand)
 
@@ -75,7 +75,7 @@ class NativeEdtCliConverter implements IEdtCliEngine {
 
         Logger.println("Конвертация исходников из формата конфигуратора в формат EDT с помощью 1cedtcli")
 
-        def edtcliCommand = "1cedtcli -data \"$workspaceDir\" -vmargs -Xmx8g -command import --configuration-files \"$configurationRoot\" --project-name \"$projectName\""
+        def edtcliCommand = "1cedtcli -data \"$workspaceDir\" -vmargs -Xmx8g -DdisableProjectChecks=true -DdisableFTS=true -command import --configuration-files \"$configurationRoot\" --project-name \"$projectName\""
 
         steps.cmd(edtcliCommand)
 
@@ -89,7 +89,7 @@ class NativeEdtCliConverter implements IEdtCliEngine {
         String workspaceLocation = "$env.WORKSPACE/$DesignerToEdtFormatTransformation.WORKSPACE"
         def resultFile = "$env.WORKSPACE/$EdtValidate.RESULT_FILE"
 
-        def edtcliCommand = "1cedtcli -data \"$workspaceLocation\" -vmargs -Xmx8g -command validate --file \"$resultFile\" $projectList"
+        def edtcliCommand = "1cedtcli -data \"$workspaceLocation\" -vmargs -Xmx8g -DdisableProjectChecks=true -DdisableFTS=true -command validate --file \"$resultFile\" $projectList"
         steps.catchError {
             steps.cmd(edtcliCommand)
         }

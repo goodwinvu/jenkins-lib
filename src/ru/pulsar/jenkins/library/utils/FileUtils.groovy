e620cec981798cb6542fe8e83455eac7a059d352
v.belyaev <v.belyaev@1ceasy.ru>
2026-09-04 12:24:49 +0300

    слияние с апстрим

--- src/ru/pulsar/jenkins/library/utils/FileUtils.groovy
+++ src/ru/pulsar/jenkins/library/utils/FileUtils.groovy
@@ -59,7 +59,6 @@ class FileUtils {
             localPathToFile.copyFrom(localFilePath)
         }
     }
-
     private static boolean isValidUrl(String url) {
         try {
             new URL(url).toURI()
